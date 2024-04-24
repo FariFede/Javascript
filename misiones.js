cadbd438f69d2025c7fcbe4cbbdde211349df155
FariFede <92767598+FariFede@users.noreply.github.com>
2024-04-24 20:20:51 -0300

    2 - Corrección Issue #1
    Modificación de nombre de clases.
    Desglose del parámetro "detalle" en "custodia" para la subclase "MisionPrincesa" y "distancia" para la subclase "MisionObjeto".
    Eliminación de clase "ListaMisiones".
    Creación de métodos "quienSolicita()" y "verSolicitante()" dentro de clases "Mision" y "Heroe", respectivamente.

--- misiones.js
+++ misiones.js
@@ -1,88 +1,95 @@
 /* mi heroe debe saber hacer misiones */
 
-class mision {
+class Mision {
     constructor(tipo, solicitante, detalle) { // hay 2 tipos de misiones, podrian ser mas a futuro. necesitamos el solicitante y un campo detalle para calcular la recompensa
         this.tipo = tipo;
         this.solicitante = solicitante;
         this.detalle = detalle;
     }
- 
-    esDificil() { // hay misiones normales y misiones dificiles, unificamos criterios
-        return this.solicitante.charAt(0) === 'G'  &&
-        (this.detalle === 4 || this.detalle === 5 || this.detalle > 100);
+
+    quienSolicita() { // identifico al solicitante dentro de mi mision
+        return this.solicitante;
+    }
+
+    esDificil() { // hay misiones normales y misiones dificiles, las cuales se dan en condiciones especificas de las subclases
+        return false;
     }                               
- 
+
     puntos() { // en los puntos no unifique los criterios, pero al igual que en el metodo esDificil, podria hacerlo con las 2 existentes
         return 0; 
     }
 }
- 
- 
 
-class misionPrincesa extends mision { // mision especifica de rescatar a una princesa
 
-    constructor(tipo, solicitante, detalle) { // el constructor podria no ir, pero lei que es conveniente por si en un futuro se complejiza con distintos metodos para el heroe
-        super(tipo, solicitante, detalle);
+class MisionPrincesa extends Mision { // mision especifica de rescatar a una princesa
+
+    constructor(tipo, solicitante, custodia) {
+        super(tipo, solicitante, custodia);
+        this.custodia = custodia;
+    }
+
+    esDificil() {
+        return this.solicitante.charAt(0) === 'G'  &&
+        (this.custodia === 4 || this.custodia === 5);
     }
 
     puntos() {
         return this.detalle * 2;
     }
 }
- 
- 
 
-class misionObjeto extends mision {
 
-    constructor(tipo, solicitante, detalle) { // el constructor podria no ir, pero lei que es conveniente por si en un futuro se complejiza con distintos metodos para el heroe
-        super(tipo, solicitante, detalle);
+class MisionObjeto extends Mision {
+
+    constructor(tipo, solicitante, distancia) {
+        super(tipo, solicitante, distancia);
+        this.distancia = distancia;
+    }
+
+    esDificil() {
+        return this.solicitante.charAt(0) === 'G'  &&
+        (this.distancia > 100);
     }
 
     puntos() {
         return this.detalle <= 50 ? 10 : 5;
     }
 }
- 
-/* esas misiones que mi heroe sabe hacer se van agregando a su lista */
 
-class listaMisiones { // lista de misiones
+
+/* defino a mi heroe, el que hace las misiones. ahora mi heroe tiene su propia lista, y sabe recurrir a la clase mision para filtrar si la mision que le asignaron es dificil o no */
+
+class Heroe {
     constructor() {
-        this.misiones = [];
-    }
- 
-    agregarMision(mision) { // metodo con el que agrego misiones a la lista de mi heroe
-        this.listaMisiones.push(mision);
-    }
- 
-    misionDificil() { // puedo filtrar por dentro de la lista para conocer las misiones dificiles, devuelve un array
-        return this.listaMisiones.filter(mision => mision.esDificil());
+        this.misiones = []; // lista de misiones del heroe en cuestion
     }
 
-}
- 
-
-/* defino a mi heroe, el que hace las misiones */
- 
-class heroe {
-    constructor() {
-        this.misiones = new listaMisiones(); // lista de misiones del heroe en cuestion
-    }
- 
     hacerMision(mision) {
         this.misiones.agregarMision(mision);
     }
- 
+
     misionDificil() {
-        return this.misiones.misionDificil(); // misiones dificiles asignadas al heroe en cuestion
+        return this.misiones.filter(mision => mision.esDificil()); // misiones dificiles asignadas al heroe en cuestion
     }
- 
+
+    verSolicitante() {
+        return this.misiones.map(mision => mision.quienSolicita()); // uso map para crear un nuevo array con los resultados de mision, afectados por el metodo quienSolicita
+    }
+
     totalPuntos() { // suma de puntos de recompensa de la lista de misiones del heroe
         return this.misiones().reduce((total, mision) => total + mision.puntos(), 0);
     }
 }
- 
- 
-const Shrek = new heroe();
 
-Shrek.hacerMision(new misionPrincesa('Princesa', 'Lord Farquaad', 5));
-Shrek.hacerMision(new misionObjeto('Objeto', 'Gandalf', 120));
+
+
+const Shrek = new Heroe();
+
+Shrek.hacerMision(new MisionPrincesa('Princesa', 'Lord Farquaad', 5));
+Shrek.hacerMision(new MisionObjeto('Objeto', 'Gandalf', 120));
+
+/* misiones dificiles de Shrek */
+console.log('Misiones difíciles asignadas a Shrek:', Shrek.misionDificil());
+
+/* lista completa de solicitantes */
+Shrek.verSolicitante().forEach(solicitante => {console.log(`Solicitante de la misión: ${solicitante}`);});
